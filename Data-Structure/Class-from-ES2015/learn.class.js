86427bcde532f3291c796274cbbe41d2b02b261c
EG-DEV5 <egypt.team.dev5@gmail.com>
2022-03-02 15:47:34 +0200

    another shoot in class

--- Data-Structure/Class-from-ES2015/learn.class.js
+++ Data-Structure/Class-from-ES2015/learn.class.js
@@ -1,10 +1,11 @@
 // ? create a class
 class Student {
-  constructor(fName, lName, year = 1889) {
+  constructor(fName, lName, year) {
     this.firstName = fName;
     this.lastName = lName;
     this.grade = year;
     this.tardies = 0;
+    this.scores = [];
   }
 
   fullName() {
@@ -12,13 +13,32 @@ class Student {
   }
 
   markLate() {
-    this.tardies += 2;
-    console.log(this.tardies);
+    this.tardies = this.tardies + 1;
+    if (this.tardies >= 3) return "YOU ARE EXPELED!!!!!";
+
     return `${this.firstName} ${this.lastName} has been late ${this.tardies} times!`;
+  }
+
+  addScore(score) {
+    this.scores.push(score);
+    return this.scores;
+  }
+
+  calculateAvg() {
+    let sum = this.scores.reduce((a, b) => a + b, 0);
+    return sum / this.scores.length;
+  }
+
+  static enrollStudents() {
+    return "hello from the other side,,,";
   }
 }
 
 const student = new Student("Abduallah", "Mohamed", 2020);
-const student2 = new Student("Abduallah", "Mohamed");
+const student2 = new Student("mohamed", "sarhan");
 
 console.log(student.markLate());
+console.log(student.addScore(1));
+console.log(Student.enrollStudents());
+
+// console.log(student2.markLate());
